05490e07126eb9f2904fad201a4f012b6b5577c7
ouillliam <62510602+ouillliam@users.noreply.github.com>
2023-03-29 12:01:51 +0200

    leave ok

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from dht import Node, DHT, MessageType, Message, Data
 import simpy
 import logging
 
-
+with open('logs.txt', 'w+'):
+    pass
 
 env = simpy.Environment()
-logging.basicConfig(filename='logs.txt', filemode='w', format='%(levelname)s - %(message)s', level=logging.DEBUG)
+logging.basicConfig(filename='logs.txt', filemode='w', format='%(levelname)s - %(message)s')
 num_nodes = 5
 dht = DHT(env)
 for i in range(num_nodes):
@@ -13,10 +14,16 @@ for i in range(num_nodes):
     env.run()
 node = dht.create_node(env, 50)
 env.run()
-dht.store_data(env, 'test', 25)
+node = dht.create_node(env, 17)
 env.run()
-node = node = dht.create_node(env, 17)
+dht.store_data(env, 'test', 24)
 env.run()
-node.get_data(25)
+node.get_data(24)
+dht.store_data(env, 'fefe', 14)
 env.run()
+env.process(node.leave())
+env.run()
+DHT.network.pop(node.ip)
 print(dht)
+print((list( node for node in DHT.network.values() if node.id == 0))[0].data)
+
